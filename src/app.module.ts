8e066fba4085636f98d67a282fabca634fc381cd
Luan Vendt <luan.vendt@bitize.com.br>
2024-03-25 15:54:29 -0300

    Estrutura básica de orders, recipients e users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './resource/users/users.module';
+import { OrdersModule } from './resource/orders/orders.module';
+import { RecipientsModule } from './resource/recipients/recipients.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, OrdersModule, RecipientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
