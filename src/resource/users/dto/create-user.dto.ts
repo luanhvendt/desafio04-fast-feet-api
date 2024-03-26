6b356a976abd632110d06608aadba09692a423fe
Luan Vendt <luan.vendt@bitize.com.br>
2024-03-26 14:08:32 -0300

    tipando createUserDto

--- src/resource/users/dto/create-user.dto.ts
+++ src/resource/users/dto/create-user.dto.ts
@@ -1,8 +1,18 @@
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @MinLength(5)
     password: string;
+
+    @MaxLength(11)
     cpf: string;
+
     latitude: number;
     longitude: number;
     createdAt: Date;
