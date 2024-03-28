4a41b7a1207691c10bc0d6e84f5dde333f87131c
Luan Vendt <luan.vendt@bitize.com.br>
2024-03-28 16:55:01 -0300

    testes e2e de recipients

--- src/resource/recipients/recipients.controller.spec.ts
+++ src/resource/recipients/recipients.controller.spec.ts
@@ -1,20 +1,109 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { RecipientsController } from './recipients.controller';
-import { RecipientsService } from './recipients.service';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from '../../app.module';
+import { PrismaService } from '../../database/prisma.service';
+import { RecipientsModule } from './recipients.module';
+
+let testAccessToken
 
 describe('RecipientsController', () => {
-  let controller: RecipientsController;
+    let app: INestApplication
+    let prisma: PrismaService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [RecipientsController],
-      providers: [RecipientsService],
-    }).compile();
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule, RecipientsModule],
+        }).compile()
 
-    controller = module.get<RecipientsController>(RecipientsController);
-  });
+        app = moduleRef.createNestApplication()
+        prisma = moduleRef.get(PrismaService)
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+        await app.init()
+    });
+
+    afterEach(async () => {
+        await prisma.recipient.deleteMany({
+            where: { email: 'mail@mail.com' }
+        })
+    });
+
+    test('[POST] /auth/login', async () => {
+        const responseLogin = await request(app.getHttpServer())
+            .post('/auth/login')
+            .send({
+                cpf: '11111111111',
+                password: '12345'
+            })
+
+        const responseBody = JSON.parse(responseLogin.text)
+        const accessToken = responseBody.accesToken
+        testAccessToken = accessToken
+
+
+        expect(responseLogin.status).toBe(200)
+    })
+
+    test('[POST] /recipients', async () => {
+        const response = await request(app.getHttpServer())
+            .post('/recipients')
+            .set('Authorization', `Bearer ${testAccessToken}`)
+            .send({
+                name: 'name',
+                email: 'mail@mail.com'
+            })
+
+        expect(response.statusCode).toBe(201)
+    })
+
+    test('[PUT] /recipients/:id', async () => {
+        const recipient = await prisma.recipient.create({
+            data: {
+                name: 'name',
+                email: 'mail@mail.com'
+            }
+        })
+
+        const response = await request(app.getHttpServer())
+            .put(`/recipients/${recipient.id}`)
+            .set('Authorization', `Bearer ${testAccessToken}`)
+            .send({
+                name: 'name editado',
+            })
+
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('[GET] /recipients', async () => {
+        const response = await request(app.getHttpServer())
+            .get('/recipients')
+            .set('Authorization', `Bearer ${testAccessToken}`)
+            .send()
+
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('[GET] /recipients/:id', async () => {
+        const response = await request(app.getHttpServer())
+            .get('/recipients/1')
+            .set('Authorization', `Bearer ${testAccessToken}`)
+
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('[DELETE] /recipients/:id', async () => {
+        const recipient = await prisma.recipient.create({
+            data: {
+                name: 'name',
+                email: 'mail@mail.com'
+            }
+        })
+
+        const response = await request(app.getHttpServer())
+            .delete(`/recipients/${recipient.id}`)
+            .set('Authorization', `Bearer ${testAccessToken}`)
+            .send()
+
+        expect(response.statusCode).toBe(200)
+    })
 });
