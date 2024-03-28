c970138397023bb0fce9013b13516ee1a64c3e38
Luan Vendt <luan.vendt@bitize.com.br>
2024-03-28 16:16:55 -0300

    class validator em createRecipientDto

--- src/resource/recipients/dto/create-recipient.dto.ts
+++ src/resource/recipients/dto/create-recipient.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsOptional, IsString } from "class-validator";
+
 export class CreateRecipientDto {
+    @IsOptional()
+    id?: string;
+
+    @IsString()
     name: string;
+
+    @IsEmail()
     email: string;
-    createdAt: Date;
+
+    @IsOptional()
+    createdAt?: Date;
 }
--- src/resource/recipients/recipients.service.spec.ts
+++ src/resource/recipients/recipients.service.spec.ts
@@ -1,18 +1,104 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { RecipientsService } from './recipients.service';
+import { InMemoryRecipientsRepository } from "../../../test/repositories/in-memory-recipients-repository";
+import { RecipientsService } from "./recipients.service";
 
 describe('RecipientsService', () => {
-  let service: RecipientsService;
+    it('should be able to create a new recipient', async () => {
+        const inMemoryRecipientsRepository = new InMemoryRecipientsRepository()
+        const recipientsService = new RecipientsService(inMemoryRecipientsRepository)
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RecipientsService],
-    }).compile();
+        await expect(recipientsService.create({
+            name: 'name',
+            email: 'mail@mail.com',
+            createdAt: new Date(),
+        }))
+            .resolves
+            .not
+            .toThrow()
 
-    service = module.get<RecipientsService>(RecipientsService);
-  });
+        expect(inMemoryRecipientsRepository.items).toEqual(expect.arrayContaining([
+            expect.objectContaining({
+                email: 'mail@mail.com'
+            })
+        ]))
+    })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be able to find all recipients', async () => {
+        const inMemoryRecipientsRepository = new InMemoryRecipientsRepository()
+        const recipientsService = new RecipientsService(inMemoryRecipientsRepository)
+
+        await expect(recipientsService.create({
+            name: 'name',
+            email: 'mail@mail.com',
+            createdAt: new Date(),
+        }))
+            .resolves
+            .not
+            .toThrow()
+
+        await expect(recipientsService.create({
+            name: 'name2',
+            email: 'mail2@mail.com',
+            createdAt: new Date(),
+        }))
+            .resolves
+            .not
+            .toThrow()
+
+        const recipients = await inMemoryRecipientsRepository.findAll()
+
+        expect(recipients).toEqual(expect.arrayContaining([
+            expect.objectContaining({
+                email: 'mail@mail.com'
+            }),
+            expect.objectContaining({
+                email: 'mail2@mail.com'
+            })
+        ]))
+    })
+
+    it('should be able to find an unique recipient', async () => {
+        const inMemoryRecipientsRepository = new InMemoryRecipientsRepository()
+        const recipientsService = new RecipientsService(inMemoryRecipientsRepository)
+
+        await expect(recipientsService.create({
+            name: 'name',
+            email: 'mail@mail.com',
+            createdAt: new Date(),
+        }))
+            .resolves
+            .not
+            .toThrow()
+
+        await expect(recipientsService.create({
+            name: 'name2',
+            email: 'mail2@mail.com',
+            createdAt: new Date(),
+        }))
+            .resolves
+            .not
+            .toThrow()
+
+        const recipient = await inMemoryRecipientsRepository.findUniqueByEmail('mail2@mail.com')
+
+        expect(recipient).toEqual(expect.objectContaining({
+            email: 'mail2@mail.com'
+        }))
+    })
+
+    it('should be able to update a recipient', async () => {
+        const inMemoryRecipientsRepository = new InMemoryRecipientsRepository()
+        const recipientsService = new RecipientsService(inMemoryRecipientsRepository)
+
+        await expect(recipientsService.create({
+            id: 1,
+            name: 'name',
+            email: 'mail@mail.com',
+            createdAt: new Date(),
+        }))
+            .resolves
+            .not
+            .toThrow()
+
+        await expect(recipientsService.update())
+    })
 });
